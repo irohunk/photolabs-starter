56a0adecb8664ba711d4bc23cc3cdff914a4bac6
Rohan Kulkarni <irohunk@gmail.com>
2024-08-11 11:35:24 -0400

    resolved bugs pertaining to TopNavigationBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,28 @@ import React, {useState} from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import PhotoFavButton from './components/PhotoFavButton';
 import PhotoList from 'components/PhotoList';
+import TopNavigationBar from 'components/TopNavigationBar';
+import TopicList from 'components/TopicList';
 
 import './App.scss';
-import TopicList from 'components/TopicList';
-import TopNavigationBar from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 
 
 const App = () => {
+  // Sample state to manage favorite photos
+  const [favPhotos, setFavPhotos] = useState([]);
+
+  // Check if there are any favorite photos
+  const isFavPhotoExist = favPhotos.length > 0;
+
   const toggleModal = (photo) => {
     console.log('Toggled modal for', photo);
   };
 
   return (
     <div className="App">
-      <TopNavigationBar />
+      <TopNavigationBar isFavPhotoExist={isFavPhotoExist} />
       <PhotoList toggleModal={toggleModal} />
     </div>
   );
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,7 +3,7 @@ import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = ({ isFavPhotoExist }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
